a7b5d352ed229424e23520d095ac321aff30bc88
rubynovich <roman@shipiev.me>
2013-05-20 15:29:32 +0400

    fix: github -> bitbucket

--- init.rb
+++ init.rb
@@ -3,8 +3,8 @@ require 'redmine'
 Redmine::Plugin.register :redmine_recreator_issues do
   name 'Recreator issues'
   author 'Roman Shipiev'
-  description 'This plugin add new tasks for re-create issues (with reset old status, and recalculation start_date and due_date, and without attachments). Useful for repetitive issues'
+  description 'Adding new tasks for re-create issues (with reset old status, and recalculation start_date and due_date, and without attachments). Useful for repetitive issues'
   version '0.0.2'
-  url 'https://github.com/rubynovich/redmine_recreator_issues'
+  url 'https://bitbucket.org/rubynovich/redmine_recreator_issues'
   author_url 'http://roman.shipiev.me'
 end
